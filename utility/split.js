b47436d25070a3425d0ca9da88838f46383a4cde
EldySparrownest <83872453+EldySparrownest@users.noreply.github.com>
2026-05-03 11:18:36 +0200

    Update split.js to exclude invalid combos
    Within the Blue Prince game itself, it is not valid for the split parts to have leading 0s (not even 00).
    Example: 102003000 can be split as [1, 0, 200, 3000] or [10, 20, 0, 3000] or [10, 200, 300, 0] but no other way.

--- utility/split.js
+++ utility/split.js
@@ -10,9 +10,18 @@ export function split(numStr) {
         for (let j = i + 1; j <= n - 2; j++) {
             for (let k = j + 1; k <= n - 1; k++) {
                 const part1 = Number(numStr.slice(0, i));
-                const part2 = Number(numStr.slice(i, j));
-                const part3 = Number(numStr.slice(j, k));
-                const part4 = Number(numStr.slice(k));
+                
+                let strPart2 = numStr.slice(i, j);
+                if (strPart2.startsWith('0') && strPart2.length > 1) continue;
+                const part2 = Number(strPart2);
+
+                let strPart3 = numStr.slice(j, k);
+                if (strPart3.startsWith('0') && strPart3.length > 1) continue;
+                const part3 = Number(strPart3);
+
+                let strPart4 = numStr.slice(k);
+                if (strPart4.startsWith('0') && strPart4.length > 1) continue;
+                const part4 = Number(strPart4);
 
                 results.push([part1, part2, part3, part4]);
             }
